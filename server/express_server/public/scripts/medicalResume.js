dc44e5befc7d38dd785f7fc75dbb31369fc88d65
luanvill <luanvill@espol.edu.ec>
2021-01-17 14:37:02 -0500

    Consulta con BD exitosa para medicalResume

--- server/express_server/public/scripts/medicalResume.js
+++ server/express_server/public/scripts/medicalResume.js
@@ -3,6 +3,6 @@ $("#searchMedicalResume").submit(function (e) {
     postFetch("/user/medicalResume",{
         id_card_pacient: $('#id_card_pacient').val(),
     }).then((res) => {
-        console.log("Procesado post medical Resume")
+        console.log("Procesado post medical Resume",res)
     })
 });
